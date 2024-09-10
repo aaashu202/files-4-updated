d86dce97ff313bc525a75a71a55c02eb918cbb75
aaashu202 <167896518+aaashu202@users.noreply.github.com>
2024-09-10 18:26:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -51,7 +51,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", None)
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
