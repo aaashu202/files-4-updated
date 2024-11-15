7e35eacfe7a26c3d17485831e8d98505268b760e
aaashu202 <167896518+aaashu202@users.noreply.github.com>
2024-11-15 09:19:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ class Bot(Client):
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Dramafilez")
         self.LOGGER(__name__).info(f""" \n\n       
-(っ◔◡◔)っ ♥   @Dramafile    ♥
+(っ◔◡◔)っ ♥   @Dramafilez   ♥
 ░╚════╝░░╚════╝░╚═════╝░╚════╝░
                                           """)
         self.username = usr_bot_me.username
